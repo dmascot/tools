c83c41d14c46bb66a8aae37f17c00cc740ceb37b
Dhaval Vadgama <dhaval.it@gmail.com>
2023-05-16 11:25:41 +0100

    added: web installer for windows removed: dockerignore from test directory update: cosmetic changes in setup.ps1 update: README.md

--- lib/windows/tools.ps1
+++ lib/windows/tools.ps1
@@ -1,17 +1,17 @@
 . $PWD\lib\windows\functions.ps1
 
 
-function Install-Git(){
+function Install-Git() {
     #Download git scm installer
 
-    if( -not (isCommandInstalled -commandName "git" )){
-        $GIT_Base_URL ="https://git-scm.com/download/win"
+    if ( -not (isCommandInstalled -commandName "git" )) {
+        $GIT_Base_URL = "https://git-scm.com/download/win"
         $Req = Invoke-WebRequest -UseBasicParsing -Uri $GIT_Base_URL
-        $DownloadLink =  ($req.Links | Where-Object outerhtml -match "64-bit Git for Windows Setup").href
+        $DownloadLink = ($req.Links | Where-Object outerhtml -match "64-bit Git for Windows Setup").href
 
         $outFile = Get-Web-File -Url $DownloadLink
-        
-        if ( -not ( Test-Path -Path $outFile -PathType Leaf ) ) { throw "File Does not exist"}
+
+        if ( -not ( Test-Path -Path $outFile -PathType Leaf ) ) { throw "File Does not exist" }
 
         Start-Process $outFile -ArgumentList "/SILENT" -Wait
 
@@ -27,25 +27,24 @@ function Get-PoshGit {
 
     $profile_string = "Import-Module $module_name"
 
-    if ( -not (isModuleInstalled -ModuleName $module_name)) 
-    { 
-        Get-Module -ModuleName $module_name     
+    if ( -not (isModuleInstalled -ModuleName $module_name)) {
+        Get-Module -ModuleName $module_name
     }
-    else { 
+    else {
         Write-Information "$module_name is already installed"
     }
 
-    if ( -not (Test-Path -Path $PROFILE -PathType Leaf )){
+    if ( -not (Test-Path -Path $PROFILE -PathType Leaf )) {
         New-Item -ItemType File -Path $PROFILE -Force
     }
 
     #If import-module string does not exist in the Profile add it
     $porifle_string_status = Select-String -Path $PROFILE -Pattern $profile_string
-    if ( $null -eq $porifle_string_status){
+    if ( $null -eq $porifle_string_status) {
         $current_content = Get-Content $PROFILE
         $new_content = New-Object System.Collections.ArrayList
 
-        foreach($content in $current_content){
+        foreach ($content in $current_content) {
             $new_content.Add($content)
         }
 
@@ -60,24 +59,26 @@ function Get-PoshGit {
 function Install-NVM {
     # Install NVM and, set the path
 
-    if(-not(isCommandInstalled -commandName "nvm" )) {
+    if (-not(isCommandInstalled -commandName "nvm" )) {
         $BASE_URL = "https://github.com"
-        $NVM_PATH = "/coreybutler/nvm-windows/releases/latest"
+        $NVM_URL = $BASE_URL + "/coreybutler/nvm-windows/releases/latest"
+        $DOWNLOAD_FILE = "nvm-setup.exe"
+        $DOWNLOAD_URL = $BASE_URL + "/coreybutler/nvm-windows/releases/download/"
 
-        $URI =  [Uri]::new([Uri]::new($BASE_URL), $NVM_PATH).ToString()
+        $Req = Invoke-WebRequest -UseBasicParsing  -Uri $NVM_URL
 
-        $Req = Invoke-WebRequest -UseBasicParsing  -Uri $URI
+        $LATEST_VERSION = $Req.BaseResponse.RequestMessage.RequestUri.Segments[-1]
 
-        $Download_Part = ($Req.Links | Where-Object outerhtml -match "nvm-setup.exe").href
+        $DownloadLink = $DOWNLOAD_URL.Trim("/") + "/" + $LATEST_VERSION.Trim("/") + "/" + $DOWNLOAD_FILE.Trim("/")
 
-        $DownloadLink =  [Uri]::new([Uri]::new($BASE_URL), $Download_Part).ToString()
+        Write-Output $DownloadLink
 
         $outFile = Get-Web-File -Url $DownloadLink
-        
-        if ( -not ( Test-Path -Path $outFile -PathType Leaf ) ) { throw "File Does not exist"}
+
+        if ( -not ( Test-Path -Path $outFile -PathType Leaf ) ) { throw "File Does not exist" }
 
         Unblock-File $outFile
-        Start-Process $outFile -ArgumentList "/SILENT" -Wait        
+        Start-Process $outFile -ArgumentList "/SILENT" -Wait
 
         Set-UserEnvironmentPath -Path "$env:HOME\AppData\Roaming\nvm"
         Set-UserEnvironmentPath -Path "$env:ProgramFiles\nodejs"
@@ -93,9 +94,9 @@ function Install-NVM {
 function Install-Pyenv {
     #install pywin-env
 
-    if(-not (isCommandInstalled -commandName "pyenv") ){
+    if (-not (isCommandInstalled -commandName "pyenv") ) {
         $DownloadLink = "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1"
-        
+
         $outFile = Get-Web-File -Url $DownloadLink
 
         & $outFile
